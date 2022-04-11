89bf5b44654dd5b731f751e09b3e9e8777881264
Nicolò Valenza <nico.vale.spazio@gmail.com>
2022-04-11 17:21:59 +0200

    only site

--- arduino/base/secret.h
+++
@@ -1,4 +0,0 @@
-#define WIFI_SSID ""  //inserire il nome della WiFi
-#define WIFI_PASSWORD ""  //inserire la password della WiFi
-#define FIREBASE_HOST ""  //inserire il dominio del DB Firebase
-#define FIREBASE_AUTH "" //inserire il "secret" del DB Firebase
