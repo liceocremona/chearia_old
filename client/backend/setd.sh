da237b6c723efb2e19bcf85eab5ea9292dbcb408
Nicolò Valenza <nico.vale.spazio@gmail.com>
2022-04-18 01:36:12 +0200

    fix api

--- client/backend/setd.sh
+++ client/backend/setd.sh
@@ -1,1 +1,1 @@
-sudo certbot --nginx -d progettochearia.it -d www.progettochearia.it -d web.progettochearia.it -d api.progettochearia.it -d storage.progettochearia.it
+sudo certbot --nginx -d progettochearia.it -d www.progettochearia.it -d web.progettochearia.it -d api.progettochearia.it -d storage.progettochearia.it -d rstudio.progettochearia.it
