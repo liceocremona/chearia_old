bc02f08449a1d7a93df4b754cc7e782a3af9508d
pc-progetto-chearia <github@iiscremona.it>
2022-05-16 13:13:41 +0200

    r

--- arduino/fly-slave_notime/secret.h
+++ arduino/fly-slave_notime/secret.h
@@ -1,3 +1,3 @@
-#define WIFI_SSID "ManuValeWiFi_plus"  //inserire il nome della WiFi
-#define WIFI_PASSWORD "Soyuz111"  //inserire la password della WiFi
-#define KEY "JaNdRgUkXp2s5v8y/A?D(G+KbPeShVmY" //inserire il "secret" del DB Firebase
+#define WIFI_SSID ""  //inserire il nome della WiFi
+#define WIFI_PASSWORD ""  //inserire la password della WiFi
+#define KEY "" //inserire il "secret" del DB Firebase
