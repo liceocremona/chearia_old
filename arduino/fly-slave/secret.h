28afd133c0f60fb6d83dfdc1e698487c1feef49f
Nicolò Valenza <nico.vale.spazio@gmail.com>
2022-06-14 01:35:43 +0200

    Documentation (#2)
    * add first docs
    
    * add flow1
    
    Co-authored-by: pc-progetto-chearia <github@iiscremona.it>

--- arduino/fly-slave/secret.h
+++
@@ -1,4 +0,0 @@
-#define WIFI_SSID "Modem 4G Wi-Fi_6E15"  //inserire il nome della WiFi
-#define WIFI_PASSWORD "24636304"  //inserire la password della WiFi
-#define FIREBASE_HOST "olimpiadirobotica-2021-default-rtdb.europe-west1.firebasedatabase.app"  //inserire il dominio del DB Firebase
-#define FIREBASE_AUTH "qyi23POQvYs4zZb0pnrlCoVVUdNLXLmbdatb028c" //inserire il "secret" del DB Firebase
